76e0705f0c18a05ea3f441370b09ed2e5d0ef2eb
Wiggins <wiggins@concentricsky.com>
2013-05-17 14:32:45 -0700

    navigation drawer stub

--- thoth/src/main/java/com/concentricsky/android/thoth/ThothMainActivity.java
+++ thoth/src/main/java/com/concentricsky/android/thoth/ThothMainActivity.java
@@ -1,12 +1,26 @@
 package com.concentricsky.android.thoth;
 
+import android.app.ActionBar;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
 public class ThothMainActivity extends Activity {
+    private ActionBar mActionBar;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ThothActionBarDrawerToggle mDrawerToggle;
+
 
     private RequestQueue mRequestQueue;
 
@@ -15,8 +29,32 @@ public class ThothMainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //set up navigation drawer
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView)findViewById(R.id.navigation_drawer);
+
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+//        mDrawerList.setAdapter();
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        mActionBar = getActionBar();
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+        mActionBar.setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ThothActionBarDrawerToggle();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+
+       if (savedInstanceState == null) {
+           //initial startup
+       }
+
+
+
         //http://www.youtube.com/watch?v=yhv8l9F44qo#t=14m36
-        mRequestQueue = Volley.newRequestQueue(this);
+//        mRequestQueue = Volley.newRequestQueue(this);
+
+
     }
 
 
@@ -26,5 +64,75 @@ public class ThothMainActivity extends Activity {
         getMenuInflater().inflate(R.menu.thoth_main, menu);
         return true;
     }
-    
+
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean is_open = mDrawerLayout.isDrawerOpen(mDrawerList);
+
+        //hide other actionbar items when drawer is visible
+//        menu.findItem(R.id.action_sync).setVisible(!is_open);
+//        menu.findItem(R.id.action_add).setVisible(!is_open);
+//        menu.findItem(R.id.action_share).setVisible(!is_open);
+//        menu.findItem(R.id.action_goto).setVisible(!is_open);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        switch (item.getItemId()) {
+        //case R.id.action_sync:
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    /*
+     * ActionBarDrawerToggle needs these
+     */
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    /*
+     * Private Classes
+     */
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        }
+    }
+
+    private class ThothActionBarDrawerToggle extends ActionBarDrawerToggle {
+        public ThothActionBarDrawerToggle() {
+            super(ThothMainActivity.this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close);
+        }
+        @Override
+        public void onDrawerClosed(View drawerView) {
+            invalidateOptionsMenu();
+        }
+
+        @Override
+        public void onDrawerOpened(View drawerView) {
+//            mActionBar.setTitle(mDrawerTitle)
+            invalidateOptionsMenu();
+        }
+    }
+
+
 }
